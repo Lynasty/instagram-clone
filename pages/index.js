cd5f7152f7fd4666b5faca5732097a3f11e95958
Darwin <darwin_95@msn.com>
2021-12-03 18:55:41 +0100

    Stories Feed with Faker

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Header from "../components/Header";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Instagram 2.0</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+            <Head>
+                <title>Instagram 2.0</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      {/* Header */}
-      <Header />
+            {/* Header */}
+            <Header />
 
-      {/* Feed */}
-
-      {/* Modal */}
-    </div>
-  )
+            {/* Feed */}
+            <Feed />
+            {/* Modal */}
+        </div>
+    );
 }
