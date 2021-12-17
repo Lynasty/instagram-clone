ac3fc55cdde9f50b736ea2ed7b71bed622a51c2e
Darwin <darwin_95@msn.com>
2021-12-17 18:47:37 +0100

    Mobile menu

--- components/Header.js
+++ components/Header.js
@@ -19,8 +19,8 @@ function Header() {
     const router = useRouter();
 
     return (
-        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto px-4 xl:px-0">
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50 py-2">
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto lg:px-4 xl:px-0">
                 {/* Left */}
                 <div onClick={() => router.push('/')} className="relative hidden lg:inline-grid w-24 cursor-pointer">
                     <Image
@@ -37,8 +37,8 @@ function Header() {
                     />
                 </div>
                 {/* Middle */}
-                <div className="max-w-sm">
-                    <div className="relative mt-1 p-3 rounded-md">
+                <div className="max-w-sm hidden md:block">
+                    <div className="relative rounded-md">
                         <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                             <SearchIcon className="h-5 w-5 text-gray-500" />
                         </div>
@@ -75,7 +75,6 @@ function Header() {
                     ) : (
                         <button onClick={signIn}>Sign in</button>
                     )}
-                    <MenuIcon className="h-6 md:hidden cursor-pointer" />
                 </div>
             </div>
         </div>
